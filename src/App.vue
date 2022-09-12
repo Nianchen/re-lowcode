ec640f2337b8e8c7542a8e8b50728269d3c183d6
Nianchen <2216092064@qq.com>
2022-09-12 16:22:21 +0800

    基本UI框架代码

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+      <AppHome></AppHome>
+  </div>
+</template>
+
+<script>
+import AppHome from '@/Views/AppHome.vue'
+export default {
+  name: 'App',
+  components: {
+    AppHome
+  }
+}
+</script>
+
+<style>
+html,body,#app{
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/Views/AppHome.vue
@@ -0,0 +1,51 @@
+<template>
+  <el-container>
+    <el-aside style="width: 180px">
+      <div>left菜单</div>
+    </el-aside>
+    <el-container>
+      <el-header>Head</el-header>
+      <el-main>内容区域</el-main>
+      <el-footer>footer</el-footer>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: "AppHome",
+};
+</script>
+
+<style scoped>
+.el-container {
+  height: 100%;
+}
+
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+.el-menu {
+  background-color: #d3dce6;
+}
+</style>
+  
